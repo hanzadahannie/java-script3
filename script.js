3f9898d3dd7f3487b634f69748f9decde3fbdc15
hanzadahannie <hanzadaakylbekovna@mail.com>
2023-07-12 17:18:10 +0600

    hm work

---
+++ script.js
@@ -0,0 +1,141 @@
+
+// function greetingFunction(user, age,city){
+//     if (user === 'hanzada'){
+//         if (age >= 18 && age <= 50) {
+//             if (city === 'Bishkek') {
+//                 return 'I dont know you'
+//             } else {
+//                 return   'Age must be over 18 and under 50'
+//             }
+//         }else{
+//             return  'this is not your city'
+//         }
+//     }
+// }
+//
+//  console.log(greetingFunction('hanzada',28, 'Bishkek'))
+
+// user admin true welcome, 'user'
+// false not correct user
+
+// function falseTrue(user) {
+//     if (user==='admin'){
+//         return'welcome'
+// } else {
+//     return'not correct'
+// }
+// }
+
+
+// console.log(falseTrue('admin'))
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+// function falseTrue(day) {
+//     if (day >=1 && day <=28) {
+//         return 'today is ${8} of february'
+//     }else{
+// return 'on february only 28 day'
+// }
+// }
+
+// console.log(falseTrue('29'))
+
+// 'age' must be a number ('20')
+
+// function equals(num) {
+//     if (num === 20 && typeof"number") {
+//         return ` ${num} is equals of 20`
+//     } else {
+//         return ` ${num} is not correct`
+//     }
+// }
+//
+// console.log(equals(20))
+
+
+// 'user' 'age' old, welcome
+// 'user' 'age' old, rejected
+
+// function userfunction(user,age) {
+//     if (user==="Misha", age<=18){
+//         return'welcom'
+//     } else{
+//         return 'rejected'
+//     }
+// }
+//
+// console.log(userfunction('Misha',8))
+
+// 'user' name is too short (min 3)
+// 'user' name is accepted
+
+// 'user' name is too long (max 5)
+// 'user' name is accepted
+// function userName(user,age) {
+//     if(user.length >=3 && user.length <6){
+//         return'true'
+//     }else {
+//         return 'false'
+//     }
+// }
+//
+// console.log(userName('han'))
+
+// 'num' is equals of 50
+// 'num' is not correct
+// function numEquals(num){
+//     if (num===50){
+//         return'is equals'
+    // }else{
+    //     return 'is not correct'
+    // }
+// }
+//
+// console.log(numEquals(50))
+
+// 'age' must be a string (20)
+
+
+ // function equals(age) {
+ //     if (typeof age == "string") {
+ //         return'tru'
+ //     }else {
+ //         return 'must be a string'
+ //     }
+ // }
+//
+// console.log(equals('20'))
+
+// 'age' must be older then 23
+// function ageOlder(age){
+//     if (age>23){
+//         return'welcome'
+//     }else{
+//         return ' must be older then 23'
+//     }
+// }
+//
+// console.log(ageOlder(24))
+
+// today is monday/tuesday/wednesday/thursday/friday weekend
+// max days of weeks 7
+
+// function daysofWeeks(day){
+//     if(day === 1){
+//         return'today is monday'
+//     }else if(day===2){
+//         return 'today is tuesday'
+//     } else if(day===3){
+//         return 'today is wednesday'
+//     }else if(day===4){
+//         return 'today is thursday'
+//     }else if(day===5){
+//         return 'today is friday'
+//     } else if(day=== 6,7){
+//         return 'weekend'
+//     }else{
+//         return 'max days of weeks 7 '
+//     }
+// }
+//
+// console.log(daysofWeeks(7))
