3878d12d5fdc889b8f60d7127973595dc20b497d
hanzadahannie <hanzadaakylbekovna@mail.com>
2023-07-13 22:49:06 +0600

    short plas hm

--- script.js
+++ script.js
@@ -19,10 +19,10 @@
 // false not correct user
 
 // function falseTrue(user) {
-//     if (user==='admin'){
-//         return'welcome'
+//     if (user ==='admin'){
+//         return 'welcome'
 // } else {
-//     return'not correct'
+//     return 'not correct'
 // }
 // }
 
@@ -31,7 +31,7 @@
 // today is 'day' of february
 // on february only 28 day, your day is 'day' of another month
 // function falseTrue(day) {
-//     if (day >=1 && day <=28) {
+//     if (day >= 1 && day <= 28) {
 //         return 'today is ${8} of february'
 //     }else{
 // return 'on february only 28 day'
@@ -56,15 +56,15 @@
 // 'user' 'age' old, welcome
 // 'user' 'age' old, rejected
 
-// function userfunction(user,age) {
-//     if (user==="Misha", age<=18){
-//         return'welcom'
+// function userFunction(user,age) {
+//     if (user==="Misha", age >= 18){
+//         return 'welcom'
 //     } else{
 //         return 'rejected'
 //     }
 // }
 //
-// console.log(userfunction('Misha',8))
+// console.log(userFunction('Misha',8))
 
 // 'user' name is too short (min 3)
 // 'user' name is accepted
@@ -72,8 +72,8 @@
 // 'user' name is too long (max 5)
 // 'user' name is accepted
 // function userName(user,age) {
-//     if(user.length >=3 && user.length <6){
-//         return'true'
+//     if(user.length > = 3 && user.length < 6){
+//         return 'true'
 //     }else {
 //         return 'false'
 //     }
@@ -84,8 +84,8 @@
 // 'num' is equals of 50
 // 'num' is not correct
 // function numEquals(num){
-//     if (num===50){
-//         return'is equals'
+//     if (num === 50){
+//         return 'is equals'
     // }else{
     //     return 'is not correct'
     // }
@@ -98,7 +98,7 @@
 
  // function equals(age) {
  //     if (typeof age == "string") {
- //         return'tru'
+ //         return 'tru'
  //     }else {
  //         return 'must be a string'
  //     }
@@ -106,32 +106,32 @@
 //
 // console.log(equals('20'))
 
-// 'age' must be older then 23
+// 'age' must be older than 23
 // function ageOlder(age){
-//     if (age>23){
-//         return'welcome'
+//     if (age > 23){
+//         return 'welcome'
 //     }else{
-//         return ' must be older then 23'
+//         return ' must be older than 23'
 //     }
 // }
 //
-// console.log(ageOlder(24))
+// console.log(ageOlder( 24 ))
 
 // today is monday/tuesday/wednesday/thursday/friday weekend
 // max days of weeks 7
 
-// function daysofWeeks(day){
+// function dayWeeks(day){
 //     if(day === 1){
-//         return'today is monday'
-//     }else if(day===2){
+//         return 'today is monday'
+//     }else if(day === 2){
 //         return 'today is tuesday'
-//     } else if(day===3){
+//     } else if(day === 3){
 //         return 'today is wednesday'
-//     }else if(day===4){
+//     }else if(day === 4){
 //         return 'today is thursday'
-//     }else if(day===5){
+//     }else if(day === 5){
 //         return 'today is friday'
-//     } else if(day=== 6,7){
+//     } else if(day ===6 || day === 7){
 //         return 'weekend'
 //     }else{
 //         return 'max days of weeks 7 '
